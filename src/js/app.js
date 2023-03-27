b06f54b2435568eaa4c8a27e8182b37f0cb85602
rescue3112 <spasatel.3112@gmail.com>
2023-03-27 19:36:36 +0300

    1 commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// TODO: write your code here
+const game = new Game();
+game.start();
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+// import sum from './basic';
+
+console.log('worked');
+
+// console.log(sum([1, 2]));
